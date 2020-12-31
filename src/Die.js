e987709d5ab7653b5aeedbe5933a2059348ac2df
FrogBoss74 <hervebonnel1974@gmail.com>
2021-01-01 02:23:55 +0800

    1 file modified, 6 added, 6 deleted

--- src/Die.js
+++ src/Die.js
@@ -1,18 +1,35 @@
 import React from "react";
+import img1 from "./imgs/1.png";
 
-export default function Die(props){
-
-  return(
-     <g>
-     <text x = {props.index+"em"} 
-           dx = "0.1em" 
-           dy = "1em"
-           key={"d6"+props.index} 
-           id={"d"+props.index} 
-           className="DiceRoll" 
-           onClick={(e)=> props.handleClickHold(e,props.index)}>
-     {props.val}
-     </text>
-     </g>
-  )
+export default function Die(props) {
+  return (
+    <g>
+      <image
+        src={img1}
+        height="50"
+        width="50"
+        x={props.index * 50 + "px"}
+        dx="0.1em"
+        dy="1em"
+        key={"d6img" + props.index}
+        id={"dimg" + props.index}
+        className="DiceRoll"
+        onClick={e => props.handleClickHold(e, props.index)}
+      />
+      /*
+      <text
+        x={props.index + "em"}
+        dx="0.1em"
+        dy="1em"
+        key={"d6" + props.index}
+        id={"d" + props.index}
+        className="DiceRoll"
+        display="none"
+        onClick={e => props.handleClickHold(e, props.index)}
+      >
+        {props.val}
+      </text>
+      */
+    </g>
+  );
 }
